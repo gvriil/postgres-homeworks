8bd774a584a5b128b4d9c688e210144dd79438f5
G <0020992@gmail.com>
2023-10-24 12:47:56 +0300

    postgres hw 17.1 we made 3 tables in pgAdmin and added data with pycharm

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY NOT NULL,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date NOT NULL,
+	notes varchar(50) NOT NULL
+	
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY NOT NULL,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+	
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY NOT NULL,
+	customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(80) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,57 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+def import_data_to_table(table_name, csv_file):
+    """
+    Импортирует данные из CSV-файла в указанную таблицу базы данных PostgreSQL.
+
+    Args:
+        table_name (str): Имя таблицы, в которую необходимо импортировать данные.
+        csv_file (str): Путь к CSV-файлу, содержащему данные для импорта.
+
+    Returns:
+        None
+    """
+    try:
+        # Устанавливаем соединение с базой данных
+        conn = psycopg2.connect(
+            host='localhost',
+            database='north',
+            user='postgres',
+            password='12345',
+            port='5433'
+        )
+
+        # Создаем курсор для выполнения SQL-запросов
+        cur = conn.cursor()
+
+        # Открываем CSV-файл для чтения
+        with open(csv_file, 'r') as file:
+            # Создаем CSV-ридер
+            reader = csv.reader(file)
+            # Считываем заголовок, который содержит названия столбцов
+            header = next(reader)
+            # Создаем строку для подстановки параметров
+            placeholders = ', '.join(['%s' for _ in header])
+            # Формируем SQL-запрос
+            query = f"INSERT INTO {table_name} ({', '.join(header)}) VALUES ({placeholders})"
+
+            # Импортируем данные из CSV-файла в таблицу
+            for row in reader:
+                cur.execute(query, row)
+
+        # Фиксируем изменения
+        conn.commit()
+        # Закрываем курсор
+        cur.close()
+        # Закрываем соединение
+        conn.close()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+# Пример использования
+import_data_to_table("customers", "north_data/customers_data.csv")
+import_data_to_table("employees", "north_data/employees_data.csv")
+import_data_to_table("orders", "north_data/orders_data.csv")
