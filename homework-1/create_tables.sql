c8a8dc5ce31f9d394f59d06e7569f20709ef70e2
G <0020992@gmail.com>
2023-10-25 13:21:39 +0300

    postgres hw 17.1 some bug fixes concerning type of data in tables and amount of symbols in raw

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,26 +1,26 @@
 CREATE TABLE employees
 (
-	employee_id int PRIMARY KEY NOT NULL,
+	employee_id int PRIMARY KEY,
 	first_name varchar(50) NOT NULL,
 	last_name varchar(50) NOT NULL,
-	title varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
 	birth_date date NOT NULL,
-	notes varchar(50) NOT NULL
+	notes text NOT NULL
 	
 );
 
 CREATE TABLE customers
 (
-	customer_id varchar(10) PRIMARY KEY NOT NULL,
-	company_name varchar(50) NOT NULL,
-	contact_name varchar(50) NOT NULL
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
 	
 );
 
 CREATE TABLE orders
 (
-	order_id int PRIMARY KEY NOT NULL,
-	customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+	order_id int PRIMARY KEY,
+	customer_id char(50) REFERENCES customers(customer_id) NOT NULL,
 	employee_id int REFERENCES employees(employee_id) NOT NULL,
 	order_date date NOT NULL,
 	ship_city varchar(80) NOT NULL
